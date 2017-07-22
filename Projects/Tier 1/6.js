2953976b81c5e2ff64e05c8a3c7cba0e43ebfcce
Jon Marshall <jonathan.marshall6@gmail.com>
2017-07-21 23:01:20 -0400

    call stack example

--- Projects/Tier 1/6.js
+++ Projects/Tier 1/6.js
@@ -1,5 +1,24 @@
-for (var i=1; i<=5; i++) {
-	setTimeout( function timer(){
-		console.log( i );
-	}, i*1000 );
+function baz() {
+    // call-stack is: `baz`
+    // so, our call-site is in the global scope
+
+    console.log( "baz" );
+    bar(); // <-- call-site for `bar`
 }
+
+function bar() {
+    // call-stack is: `baz` -> `bar`
+    // so, our call-site is in `baz`
+
+    console.log( "bar" );
+    foo(); // <-- call-site for `foo`
+}
+
+function foo() {
+    // call-stack is: `baz` -> `bar` -> `foo`
+    // so, our call-site is in `bar`
+
+    console.log( "foo" );
+}
+
+baz(); // <-- call-site for `baz`
